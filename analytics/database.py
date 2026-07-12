23504c36f4fd7d3d17a80f65c4cd64759af3c1bf
perezamp15-hue <perezamp15@gmail.com>
2026-07-11 21:30:07 -0700

    Delete analytics directory

--- analytics/database.py
+++
@@ -1,6 +0,0 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from config import DATABASE_URL
-
-engine = create_engine(DATABASE_URL)
-Session = sessionmaker(bind=engine)
